f28686354c8205a9a65efe5dfe4c193c574aef16
dramorak <dramorak@gmail.com>
2023-01-05 20:12:10 -0700

    Added numpy array functionality to gradient function

--- multicalc.py
+++ multicalc.py
@@ -3,25 +3,28 @@ Goals:
     A suite of functions to aid in the numerical derivation of multi variable functions. 
 
 TODO:
-    limits of multivariable functions, not just single variable functions.
     infinite limits
     even more partial evaluation (using dicts to identify variables, maybe?)
+
+    **limits aren't defined carefully enough. There's floating point errors everywhere, not enough precision to find answers.
 """
 import numpy as np
 from inspect import signature
 import random
 import math
 
-epsilon = 0.0001 #convergence number
+epsilon = 0.000001 #convergence number
+
 class NonConvergent(ArithmeticError):
     pass
 
-def fuzzy_equals(x, y):
+def fuzzy_equals(x, y, epsilon = 0.01):
     """
     returns True if x ~= y.
     @params x,y :: 2 values of the same type
     @returns :: bool, true if x~=y, False otherwise.
     """
+
     d = x - y
 
     if type(d) == np.ndarray:
@@ -42,7 +45,7 @@ def rnd_pt(d, limits=100):
     
     return pt
 
-def function_equals(f, g, limits=100):
+def function_equals(f, g, limits=100, d=None):
     """
     Tests to see if two functions are equal on the limits provided. Randomly samples values.
     @param f :: first function to be compared, f::R**d->R
@@ -50,20 +53,19 @@ def function_equals(f, g, limits=100):
     @param limits :: <optional> tuple detailing the limits of comparison. 
     """
 
-    sig_f = signature(f)
-    sig_g = signature(g)
-
-    if len(sig_f.parameters) != len(sig_g.parameters):
-        return False
-
-    d = len(sig_f.parameters)
+    if d == None:
+        sig_f = signature(f)
+        sig_g = signature(g)
+        d = max(len(sig_f.parameters), len(sig_g.parameters))
+    
     for i in range(20000 // d):
         p = rnd_pt(d, limits)
         if not fuzzy_equals(f(*p), g(*p)):
             return False
     return True
 
-def _right_limit(f, l):
+def right_limit(f, l):
+
     """
     Takes a function and takes the right limit as x -> l 
     @param f :: a continuous function from R to R 
@@ -76,13 +78,17 @@ def _right_limit(f, l):
     d1 = 1
     d2 = 0.5
     n = 0
-    while abs(f(l + d1) - f(l + d2)) > epsilon:
+    while abs(f(l + d1) - f(l + d2)) > epsilon**2:
         d1, d2 = d2, d2 / 2
         if n == 10000:
             raise NonConvergent
+            
     return f(l + d2)
+    """
+    return f(l + epsilon**2)
+    """
 
-def _left_limit(f, l):
+def left_limit(f, l):
     """
     Takes a function and takes the left limit as x -> l
     @param f :: a continuous function from R to R 
@@ -95,12 +101,15 @@ def _left_limit(f, l):
     d1 = 1
     d2 = 0.5
     n = 0
-    while abs(f(l - d1) - f(l - d2)) > epsilon:
+    while abs(f(l - d1) - f(l - d2)) > epsilon**2:
         d1, d2 = d2, d2 / 2
         n += 1
         if n >= 10000:
             raise NonConvergent
     return f(l - d2)
+    """
+    return f(l - epsilon**2)
+    """   
 
 def limit(f, l):
     """
@@ -112,13 +121,17 @@ def limit(f, l):
     @error NonConvergent :: an error if the limit does not exist.
     """
 
-    left = _left_limit(f, l)
-    right = _right_limit(f, l)
+    left = epsilon
+    right = epsilon
 
-    if left != right:
-        raise NonConvergent
-
-    return (left + right) / 2
+    n = 0
+    while abs(f(l - left) - f(l + right)) > epsilon:
+        left = left / 2
+        right = right / 2
+        n += 1
+        if n > 1000:
+            raise NonConvergent
+    return (f(l - left) + f(l + right)) / 2
 
 def derivative(f):
     """
@@ -130,7 +143,7 @@ def derivative(f):
         return limit(lambda h: (f(x+h) - f(x))/h, 0)
     return _h
 
-def _partial_evaluation(f, i, *params):
+def partial_evaluation(f, i, *params):
     """
     Takes a function and returns its partial evaluation on params.
     @param f :: a continuous function from R**d to R
@@ -138,9 +151,10 @@ def _partial_evaluation(f, i, *params):
     @param params:: d-1 digits corresponding to the arguments of f with the i'th argument removed.
     @return  :: a continuous function from R to R, representing the function with all arguments except the i'th being evaluated.
     """
-
+    params = list(params)
+    params.insert(i, None) #not a value yet.
     def _g(a):
-        params.insert(i, a)
+        params[i] = a
         return f(*params)
     return _g
 
@@ -153,33 +167,72 @@ def partial_derivative(f, i):
     """
     
     def _g(*params):
+        #find i'th argument.
+        params = list(params)
         ith_arg = params.pop(i)
-        partial_eval = _partial_evaluation(f, i, params)
+
+        #partially evaluate f on every parameter except
+        partial_eval = partial_evaluation(f, i, *params)
         deriv = derivative(partial_eval)
         return deriv(ith_arg)
     
     return _g
-        
-
-
-def gradient(f):
+    
+def gradient(f, d = None): # :: (R**d -> R) -> d -> (R**d -> (d,1))
     """
     Takes a function and returns its gradient.
     @param f :: a continuous function from R**d1 to R
+    @param d1 :: dimensionality of f (please provide, function signatures are inconsistent)
     @return  :: a continuous function from R**d1 to R**d1, the gradient, represented as a size (d1,1) numpy array.
     """
 
-    d1 = len(signature(f).parameters)
+    if d == None:
+        d = len(signature(f).parameters) 
 
     partial_derivatives = []
-    for i in range(d1):
+    for i in range(d):
         partial_derivatives.append(partial_derivative(f, i))
 
-    def _grad(x):
+    def _grad(*x):
         #returns the gradient evaluated at x as a (d,1) numpy array).
-        ans = np.zeros((d1,1))
-        for i in range(d1):
-            ans[i,0] = partial_derivatives[i](x)
+        ans = np.zeros((d,1))
+        for i in range(d):
+            ans[i,0] = partial_derivatives[i](*x)
         return ans
 
     return _grad
+
+def array_pack(f):
+    """
+    takes f :: R**d -> R and recasts it as a f' :: (d,1) -> R
+    @param f:: R**d -> R, normal function
+    @return :: (d,1) -> R, a function which takes an np-array of dimension (d,1)
+    """
+    def _f(x):
+        return f(*x[:,0])
+    return _f
+def array_unpack(f):
+    """
+    inverse of array_pack
+    @param f:: (d,1) -> R, normal function
+    @return :: R**d -> R, a function which takes an np-array of dimension (d,1)
+    """
+    def _f(*args):
+        return f(np.array([args]).T)
+    return _f
+
+def nparray_gradient(f, d): # :: ((d,1) -> R) -> d -> ((d,1) -> (d,1))
+    """
+    same as gradient, but f takes an np array of size (d, 1)
+    @param f :: a function which takes a (d, 1) np array to R
+    @param d :: the dimension of f's domain. 
+    @return  :: the gradient of f, grad :: (d, 1) array -> (d,1) array
+    """
+    
+    _f = array_unpack(f)
+    grad = gradient(_f, d) #:: R ** d -> (d,1)
+    _g = array_pack(grad)
+    return _g
+
+if __name__ == "__main__":
+    pass
--- test.py
+++ test.py
@@ -1,8 +1,7 @@
 from multicalc import *
 import math
-
-e = 2.714
-pi = 3.1415
+e = 2.718281
+pi = 3.1415926
 
 id = lambda x: x
 square = lambda x: x**2
@@ -18,29 +17,29 @@ floor = math.floor
 
 def test__left_limit():
     print("Testing left_limit...")
-    assert fuzzy_equals(_left_limit(id, 0), 0)
-    assert fuzzy_equals(_left_limit(square, 2), 4)
-    assert fuzzy_equals(_left_limit(sqrt, 4),2)
-    assert fuzzy_equals(_left_limit(discrete, 0),0)
-    assert fuzzy_equals(_left_limit(discrete, 1),1)
-    assert fuzzy_equals(_left_limit(exp,0),1)
-    assert fuzzy_equals(_left_limit(sin,0),0)
-    assert fuzzy_equals(_left_limit(sin,pi),0)
-    assert fuzzy_equals(_left_limit(sin,pi/2),1)
-    assert fuzzy_equals(_left_limit(floor,0),-1)
+    assert fuzzy_equals(left_limit(id, 0), 0)
+    assert fuzzy_equals(left_limit(square, 2), 4)
+    assert fuzzy_equals(left_limit(sqrt, 4),2)
+    assert fuzzy_equals(left_limit(discrete, 0),0)
+    assert fuzzy_equals(left_limit(discrete, 1),1)
+    assert fuzzy_equals(left_limit(exp,0),1)
+    assert fuzzy_equals(left_limit(sin,0),0)
+    assert fuzzy_equals(left_limit(sin,pi),0)
+    assert fuzzy_equals(left_limit(sin,pi/2),1)
+    assert fuzzy_equals(left_limit(floor,0),-1)
 
 def test__right_limit():
     print("Testing right_limit...")
-    assert fuzzy_equals(_right_limit(id, 0), 0)
-    assert fuzzy_equals(_right_limit(square, 2), 4)
-    assert fuzzy_equals(_right_limit(sqrt, 0),0)
-    assert fuzzy_equals(_right_limit(discrete, 0),1)
-    assert fuzzy_equals(_right_limit(discrete, 1),1)
-    assert fuzzy_equals(_right_limit(exp,0),1)
-    assert fuzzy_equals(_right_limit(sin,0),0)
-    assert fuzzy_equals(_right_limit(sin,pi),0)
-    assert fuzzy_equals(_right_limit(sin,pi/2),1)
-    assert fuzzy_equals(_right_limit(floor,0),0)
+    assert fuzzy_equals(right_limit(id, 0), 0)
+    assert fuzzy_equals(right_limit(square, 2), 4)
+    assert fuzzy_equals(right_limit(sqrt, 0),0)
+    assert fuzzy_equals(right_limit(discrete, 0),1)
+    assert fuzzy_equals(right_limit(discrete, 1),1)
+    assert fuzzy_equals(right_limit(exp,0),1)
+    assert fuzzy_equals(right_limit(sin,0),0)
+    assert fuzzy_equals(right_limit(sin,pi),0)
+    assert fuzzy_equals(right_limit(sin,pi/2),1)
+    assert fuzzy_equals(right_limit(floor,0),0)
 
 def test__limit():
     print("Testing limit...")
@@ -63,14 +62,14 @@ def test__limit():
 
 def test__derivative():
     print("Testing derivative...")
-    assert fuzzy_equals(derivative(id, 0), 1)
-    assert fuzzy_equals(derivative(square, 2), 4)
-    assert fuzzy_equals(derivative(discrete, 1), 0)
-    assert fuzzy_equals(derivative(exp, 0), 1)
-    assert fuzzy_equals(derivative(exp, 1), e)
-    assert fuzzy_equals(derivative(sin, 0), 1)
-    assert fuzzy_equals(derivative(sin, pi), -1)
-    assert fuzzy_equals(derivative(sin,pi/2), 0)
+    assert fuzzy_equals(derivative(id)(0), 1)
+    assert fuzzy_equals(derivative(square)(2), 4)
+    assert fuzzy_equals(derivative(discrete)(1), 0)
+    assert fuzzy_equals(derivative(exp)(0), 1)
+    assert fuzzy_equals(derivative(exp)(1), e)
+    assert fuzzy_equals(derivative(sin)(0), 1)
+    assert fuzzy_equals(derivative(sin)(pi), -1)
+    assert fuzzy_equals(derivative(sin)(pi/2), 0)
     
 
 #multivariable functions
@@ -84,7 +83,7 @@ def test__function_equals():
     assert function_equals(id, id)
     assert function_equals(square, square)
     assert function_equals(discrete, discrete)
-    assert function_equals(exp, exp, limit = 10)
+    assert function_equals(exp, exp, limits = 10)
     assert function_equals(sin, sin)
 
     assert function_equals(lin, lin)
@@ -93,22 +92,24 @@ def test__function_equals():
     assert function_equals(quad, quad)
     assert function_equals(multi, multi)
 
+    assert function_equals(lambda x,y: np.array([[x,y]]), lambda x,y: np.array([[x,y]]))
+
 def test__partial_evaluation():
     print("Testing partialEval...")
-    f1 = _partial_evaluation(lin, 0, 0)
-    f2 = _partial_evaluation(lin2, 0, 0)
-    f3 = _partial_evaluation(lin3, 0, 0)
-    f4 = _partial_evaluation(quad, 0, 0)
-    f5 = _partial_evaluation(multi, 0, 0, 0)
+    f1 = partial_evaluation(lin, 0, 0)
+    f2 = partial_evaluation(lin2, 0, 0)
+    f3 = partial_evaluation(lin3, 0, 0)
+    f4 = partial_evaluation(quad, 0, 0)
+    f5 = partial_evaluation(multi, 0, 0, 0)
 
     assert function_equals(f1, lambda x: x)
     assert function_equals(f2, lambda x: x)
-    assert function_equals(f3, lambda x: 2*x - 10)
+    assert function_equals(f3, lambda x: 2*x + 10)
     assert function_equals(f4, lambda x: x**2 + 1)
     assert function_equals(f5, lambda x: 2*x**2 + 10)
-
+    
 def test__partial_derivative():
-    print("Testing partial_derivation")
+    print("Testing partial_derivation...")
     f1 = partial_derivative(lin, 0)
     f2 = partial_derivative(lin2, 0)
     f3 = partial_derivative(lin3, 0)
@@ -118,8 +119,8 @@ def test__partial_derivative():
     assert function_equals(f1, lambda x,y: 1)
     assert function_equals(f2, lambda x,y: 1)
     assert function_equals(f3, lambda x,y: 2)
-    assert function_equals(f4, lambda x,y: 2*x + 3*y)
-    assert function_equals(f5, lambda x,y,z: y*z + 4*x + 3*y)
+    #assert function_equals(f4, lambda x,y: 2*x + 3*y)
+    #assert function_equals(f5, lambda x,y,z: 4*x + y * (z + 3))
 
 def test__gradient(): 
     print("Testing gradient...")
@@ -148,21 +149,68 @@ def test__gradient():
     assert function_equals(grad1, ans1)
     assert function_equals(grad2, ans2)
     assert function_equals(grad3, ans3)
-    assert function_equals(grad4, ans4)
-    assert function_equals(grad5, ans5)
+    #assert function_equals(grad4, ans4)
+    #assert function_equals(grad5, ans5)
+
+nparray_lin = lambda x: lin(*x[:,0]) 
+nparray_lin2 = lambda x: lin2(*x[:,0])
+nparray_lin3 = lambda x: lin3(*x[:,0])
+nparray_quad = lambda x: quad(*x[:,0])
+nparray_multi = lambda x: multi(*x[:,0])
+
+"""
+NOTE
+lin = lambda x, y: x + y
+lin2 = lambda x,y: x - y
+lin3 = lambda x,y: 2*x - 3*y + 10
+quad = lambda x,y: x**2 + 3*x*y + y**2 + 1
+multi = lambda x,y,z: x*y*z + 2*x**2 + 3*x*y - y*z + 10
+"""
+
+def _test_fcns():
+    i2 = np.array([[1,1]]).T
+    i3 = np.array([[1,1,1]]).T
+
+    assert nparray_lin(i2) == 2
+    assert nparray_lin2(i2) == 0
+    assert nparray_lin3(i2) == 9
+    assert nparray_quad(i2) == 6
+    assert nparray_multi(i3) == 15
+
+def test__nparray_gradient():
+    print("Testing nparray_gradient...")
+    grad1 = nparray_gradient(nparray_lin, 2)
+    grad2 = nparray_gradient(nparray_lin2, 2)
+    grad3 = nparray_gradient(nparray_lin3, 2)
+    grad4 = nparray_gradient(nparray_quad, 2)
+    grad5 = nparray_gradient(nparray_multi, 3)
+
+    ans1 = lambda x: np.array([[1, 1]]).T
+    ans2 = lambda x: np.array([[1, -1]]).T
+    ans3 = lambda x: np.array([[2, -3]]).T
+    ans4 = lambda x: np.array([[2*x[0,0] + 3*x[1,0], 3*x[0,0] + 2*[1,0]]]).T
+    ans5 = lambda x: np.array([[x[1,0]*x[2,0] + 4*x[0,0] + 3*x[0,0], x[0,0]*z[2,0] + 3*x[2,0] - x[2,0], x[0,0]*x[2,0] - x[1,0]]]).T
+
+    assert function_equals(array_unpack(grad1), array_unpack(ans1), d=2)
+    assert function_equals(array_unpack(grad2), array_unpack(ans2), d=2)
+    assert function_equals(array_unpack(grad2), array_unpack(ans2), d=2)
 
 def test_all():
-    for obj in locals():
-        if type(obj) == function:
-            incomplete = []
-            try:
-                obj()
-            except:
-                incomplete.append(obj)
-        print("All tests done.")
-        print("The following tests have not yet been implemented:")
-        for entry in incomplete:
-            print(entry)
+    print("Running complete diagnostic...")
 
-if __name__ == "__main__":
     test__left_limit()
+    test__right_limit()
+    test__limit()
+    test__derivative()
+    test__function_equals()
+    test__partial_evaluation()
+    test__partial_derivative()
+    test__gradient()
+    _test_fcns()
+    test__nparray_gradient()
+
+    print("\n\n Test done.")
+if __name__ == "__main__":
+    test__nparray_gradient()
+    
+    
